e62e221ecf218c9990463774279b9a8bc4666a95
HarishAdvith <harish48056@gmail.com>
2022-07-17 14:16:27 +0530

    last name added

--- newtask/src/components/UserCardDetails.js
+++ newtask/src/components/UserCardDetails.js
@@ -2,7 +2,7 @@ import React from "react";
 
 let UserCardDetails=({ emplyoeedata })=>{
     console.log(emplyoeedata);
-  const { firstName, id, phone, email } = emplyoeedata;
+  const { firstName, id, phone, email,lastName } = emplyoeedata;
   return (
     <React.Fragment>
         
@@ -11,11 +11,13 @@ let UserCardDetails=({ emplyoeedata })=>{
         <div className="card">
           <div className="card-header bg-primary">
             <p className="card-title text-center">NAME:-{firstName}</p>
+            <p className="card-title text-center">LastName:-{lastName}</p>
           </div>
           <div className="card-body bg-info">
             <p>ID:-{id}</p>
             <p>PHONE:-{phone}</p>
             <p>EMAIL:-{email}</p>
+            
           </div>
         </div>
       </div>
